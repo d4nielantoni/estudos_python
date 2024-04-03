62e89ca546f720e4e3b98d2abf66eb549b6fa9d4
d4nielantoni <danielantoni7575@gmail.com>
2024-04-02 21:07:27 -0300

    estudando funções

--- Estrutura de Dados/05 - Funções/funcoes.py
+++ Estrutura de Dados/05 - Funções/funcoes.py
@@ -49,3 +49,27 @@ def exibir_poema(data_extenso, *args, **kwargs):
     print(mensagem)
 
 exibir_poema("Sexta-Feira 25 de Agosto de 2024","asdads", "dadadasd")
+
+"""
+positional only / 
+keyword only *
+Keyword and positional only / *
+
+"""
+
+def somar(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def exibir_resultado(a, b, funcao):
+    resultado = funcao(a, b)
+    print(f"O resultado da operação é {resultado}")
+
+exibir_resultado(10, 10, somar)
+exibir_resultado(10, 10, subtracao)
+
+op = somar
+
+print(op(1,23))
