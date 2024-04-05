fa02e20bab38f9c8257bdfbf32e5e6b3a62161be
d4nielantoni <danielantoni7575@gmail.com>
2024-04-05 08:13:16 -0300

    desafio 4 code wars, tentando commitar todos os dias :)

---
+++ Code Wars/challenge004.py
@@ -0,0 +1,12 @@
+"""
+Convert a String to a Number!
+Description
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+"""
+
+def string_to_number(s):
+    return int(s)
+
+print(string_to_number("45"))
