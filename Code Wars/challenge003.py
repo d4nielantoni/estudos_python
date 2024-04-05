dee50c2ff75c7f4d71cc00e9e4d488190cde4235
d4nielantoni <danielantoni7575@gmail.com>
2024-04-05 08:07:12 -0300

    resolvendo desafio code wars

---
+++ Code Wars/challenge003.py
@@ -0,0 +1,11 @@
+"""
+You Can't Code Under Pressure #1
+
+Code as fast as you can! You need to double the integer and return it.
+"""
+
+def double_integer(i):
+    return i * 2
+
+
+print(double_integer(1))
