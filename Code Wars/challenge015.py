604b6441f09a07190afbbea6babc35ca7e750e9c
d4nielantoni <danielantoni7575@gmail.com>
2024-04-08 15:17:34 -0300

    challenge 015

---
+++ Code Wars/challenge015.py
@@ -0,0 +1,17 @@
+"""
+Third Angle of a Triangle
+
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+https://en.wikipedia.org/wiki/Triangle
+"""
+
+def other_angle(a, b):
+    c = 180 - (a + b)
+    return c
+
+print(other_angle(30,30))
