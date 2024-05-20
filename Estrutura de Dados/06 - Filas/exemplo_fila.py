42ddcc5872370e4d628d554974260b7f1d97e033
d4nielantoni <danielantoni7575@gmail.com>
2024-05-20 19:32:05 -0300

    exemplo fila

---
+++ Estrutura de Dados/06 - Filas/exemplo_fila.py
@@ -0,0 +1,35 @@
+class Fila:
+    def __init__(self):
+        self.itens = []
+
+    def adicionar_elemento(self, item):
+        self.itens.append(item)
+
+    def remover_elemento(self):
+        if not self.is_empty():
+            return self.itens.pop(0)
+        return None
+
+    def frente(self):
+        if not self.is_empty():
+            return self.itens[0]
+        return None
+
+    def is_empty(self):
+        return len(self.itens) == 0
+
+    def print_fila(self):
+        for item in self.itens:
+            print(item)
+
+# Exemplo de uso
+fila = Fila()
+fila.adicionar_elemento(1)
+fila.adicionar_elemento(2)
+fila.adicionar_elemento(3)
+fila.adicionar_elemento(4)
+print(fila.remover_elemento())
+print(fila.frente())
+print(fila.is_empty())
+
+fila.print_fila()
