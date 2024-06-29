5866124b079a39aed1921bfede69e89e53c79abf
Daniel <danielantoni7575@gmail.com>
2024-06-29 09:02:01 -0300

    github bot with pyautogui

---
+++ pyautogui/autocommit_bot.py
@@ -0,0 +1,19 @@
+import pyautogui
+from time import sleep
+
+#This code is for my monitor 
+
+pyautogui.keyDown('win')
+pyautogui.keyUp('win')
+
+sleep(1)
+pyautogui.click(1068, 1048)
+sleep(2)
+pyautogui.click(163, 320)
+sleep(1)
+pyautogui.click(1526, 260)
+sleep(3)
+pyautogui.write(' ')
+pyautogui.click(1813, 285)
+sleep(2)
+pyautogui.click(1121, 774)
---
+++ pyautogui/mouse_position.py
@@ -0,0 +1,9 @@
+from time import sleep
+import pyautogui
+
+while True:
+    x, y = pyautogui.position()
+
+    print(f"({x}, {y})") #print mouse position
+
+    sleep(2)
