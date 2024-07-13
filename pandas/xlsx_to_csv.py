c501866f9347333e7d8e587113d49de4dfa4b2ae
Daniel <danielantoni7575@gmail.com>
2024-07-13 15:30:00 -0300

    Commit diretamente da Serra da Capivara

--- pandas/xlsx_to_csv.py
+++ pandas/xlsx_to_csv.py
@@ -12,3 +12,5 @@ arquivo_csv = r"C:\Users\Daniel Antoni\Downloads\Metas_2024.csv" #aqui copiar ca
 df.to_csv(arquivo_csv, index=False)
 
 print("Planilha convertida para CSV com sucesso!")
+
+#ooioioioio
