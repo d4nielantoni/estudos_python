ed684a99e0443a8321b96606bcb3d485434c43f7
Daniel Antoni <danielantoni7575@gmail.com>
2024-07-18 10:14:35 -0300

    api pokemon

---
+++ API/pokemon_api/main.py
@@ -0,0 +1,26 @@
+import requests
+
+
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(pokemon_name):
+    url = f"{base_url}pokemon/{pokemon_name}"
+    response = requests.get(url)
+    print(response) # se a resposta for 200 significa que foi ok
+
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f"Pokemon not found")
+    
+pokemon_name = "pikachu"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info:
+    print(f"{pokemon_info['name']}")
+    print(f"{pokemon_info['id']}")
+    print(f"{pokemon_info['height']}")
+    print(f"{pokemon_info['weight']}")
+
+
