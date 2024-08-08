8ca6f4a013811ee134f4c05a705a6ef1f63da07d
Daniel <danielantoni7575@gmail.com>
2024-08-08 17:05:34 -0300

    cv2

---
+++ open_cv/test.py
@@ -0,0 +1,24 @@
+import cv2
+
+body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
+
+cap = cv2.VideoCapture(r"C:\Users\danie\Downloads\peoplewalking.mp4")
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    bodies = body_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(30, 30))
+
+    for (x, y, w, h) in bodies:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow('Frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
